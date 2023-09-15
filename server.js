47ac962de8c0332e340525516e34bb3c3395d74e
sporthq <nowak.sebastian991@gmail.com>
2023-09-15 08:12:41 +0200

    edit fakeapi

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const jsonServer = require('json-server');
 const clone = require('clone');
-const data = require('./data/questionsPL.json');
+const data = require('./db.json');
 
 const isProductionEnv = process.env.NODE_ENV === 'production';
 const server = jsonServer.create();
 
 // For mocking the POST request, POST request won't make any changes to the DB in production environment
-const router = jsonServer.router(isProductionEnv ? clone(data) : 'questionsPL.json', {
+const router = jsonServer.router(isProductionEnv ? clone(data) : 'db.json', {
 	_isFake: isProductionEnv,
 });
 const middlewares = jsonServer.defaults();
